a13ff8f3a1c71d891bffbd14bef09d5e02adcab7
Mig <r121221@yandex.ru>
2023-10-01 19:52:36 +0300

    динамический url slug

--- sitehumster/humster/urls.py
+++ sitehumster/humster/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 
     path('', views.index),
     path('cats/<int:cat_id>/', views.category),
+    path('cats/<slug:cat_slug>/', views.category_slug),
 ]
--- sitehumster/humster/views.py
+++ sitehumster/humster/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def category(request, cat_id):
     return HttpResponse(f'<h1>страница хомячки по категориям</h1> <p> Хомячок номер -<b>{cat_id}</b> </p>' )
+
+def category_slug(request, cat_slug):
+    return HttpResponse(f'<h1>страница хомячки по категориям</h1> <p>  категория -<b>{cat_slug}</b> </p>' )
