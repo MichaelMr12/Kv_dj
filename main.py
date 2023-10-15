d093f31c7f2edf87b2dd2da9e33a1184c6164334
Mig <r121221@yandex.ru>
2023-10-15 19:38:32 +0300

    render

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+result = []
+data = 'данные', '"данные, внутри которых есть запятая"', '"в этих ""данных, есть и запятая"" и кавычки"'
+line = 'Анна Павловна Иванова,"[запись 1, запись 2, запись 3]", ,2'
+sep = ','
+res = line.split(sep)
+count = 0
+for d in res:
+    if '"' in d:
+        count+=1
+        sp = d.split('"')
+        temp = ''
+        for s in sp:
+            if s != '':
+                temp+=s
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+    else:
+        result.append(d)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(result)
--- sitehumster/humster/views.py
+++ sitehumster/humster/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse('страница приложения humster' )
+    return render(request, 'humster/index.html')
+
 
 def category(request, cat_id):
-    return HttpResponse(f'<h1>страница хомячки по категориям</h1> <p> Хомячок номер -<b>{cat_id}</b> </p>' )
+    data = {'cat_id': cat_id}
+    return render(request, 'humster/cat_id.html', data)
+    # ,return HttpResponse(f'<h1>страница хомячки по категориям</h1> <p> Хомячок номер -<b>{cat_id}</b> </p>' )
+
 
 def category_slug(request, cat_slug):
-    return HttpResponse(f'<h1>страница хомячки по категориям</h1> <p>  категория -<b>{cat_slug}</b> </p>' )
+    data = {'cat_slug': cat_slug}
+    return render(request, 'humster/cat_slug.html', data)
+    # return HttpResponse(f'<h1>страница хомячки по категориям</h1> <p>  категория -<b>{cat_slug}</b> </p>' )
